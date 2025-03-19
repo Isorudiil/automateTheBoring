fe6523e955f78b2ea989f8d21a34398e363eac89
Elijah Herald <elijah.herald@gmail.com>
2025-03-19 12:44:11 -0400

    started selectiveCopy. You choose file type and location to copy from and to

---
+++ chapter10/selectiveCopy.py
@@ -0,0 +1,27 @@
+#! python3
+# selectiveCopy.py - walks through a folder tree and searches for files with
+# a certain file extension (.pdf, .jpg, etc.). Copies these files from their
+# current location to a specified new location.
+
+# thoughts: use os.walk() to to through folder tree
+# use shutil.move() to physically move the files
+# use re.compile() for a regex to find correct extension
+
+import os
+import re
+import shutil
+from pathlib import Path
+
+extension = input('What extension would you like to copy (format: .jpg)? ')
+folder = Path(input('Please provide the absolute path to the folder you want to copy from: '))
+newFolder = Path(input('Please input the absolute path to your new folder: '))
+
+fileRegex = re.compile(f'.*?{extension}')
+
+for folderName, subfolders, filenames in os.walk(folder):
+    for file in filenames:
+        foundFile = fileRegex.search(file)
+        if foundFile == None:
+            continue
+        oldfolder = Path(folderName)
+        shutil.copy(oldfolder / file, newFolder)
