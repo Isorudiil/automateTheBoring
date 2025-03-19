be7f9b20e6c2b1af3e85c7481fe69b4a876bfa8a
Elijah Herald <elijah.herald@gmail.com>
2025-03-19 13:19:53 -0400

    created fileSize, to find files larger than 100MB

---
+++ chapter10/fileSize.py
@@ -0,0 +1,16 @@
+#! python3
+# fileSize.py - walks through a folder tree and searches for files larger than 100MB
+# prints their absolute path to screen
+
+import os
+from pathlib import Path
+
+path = Path(input('Enter the absolute path of the folder you would like to check:\n'))
+
+for folderName, subfolders, filenames in os.walk(path):
+    for file in filenames:
+        folderPath = Path(folderName)
+        filesize = os.path.getsize(folderPath / file)
+        if filesize / (10**6) > 100:
+            print(folderPath / file)
+                     
