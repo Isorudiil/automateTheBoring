31b4c2f5fc8e11d4cd04da9d45a9e6548748982a
Elijah Herald <elijah.herald@gmail.com>
2025-03-19 13:21:36 -0400

    updated fileSize to accept user input for size in MB

--- chapter10/fileSize.py
+++ chapter10/fileSize.py
@@ -6,11 +6,11 @@ import os
 from pathlib import Path
 
 path = Path(input('Enter the absolute path of the folder you would like to check:\n'))
+sizeCheck = int(input('Find files larger than (value in MB):\n'))
 
 for folderName, subfolders, filenames in os.walk(path):
     for file in filenames:
         folderPath = Path(folderName)
         filesize = os.path.getsize(folderPath / file)
-        if filesize / (10**6) > 100:
+        if filesize / (10**6) > sizeCheck:
             print(folderPath / file)
-                     
